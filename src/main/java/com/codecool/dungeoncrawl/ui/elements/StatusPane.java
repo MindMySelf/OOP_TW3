e697403adc9f9af367a75e97252e0b68da439843
dudasdominik <dudasdominik3@gmail.com>
2023-04-04 19:03:57 +0200

    make changes to StatusPane and adds Inventory

--- src/main/java/com/codecool/dungeoncrawl/ui/elements/StatusPane.java
+++ src/main/java/com/codecool/dungeoncrawl/ui/elements/StatusPane.java
@@ -1,9 +1,14 @@
 package com.codecool.dungeoncrawl.ui.elements;
 
+import com.codecool.dungeoncrawl.logic.Inventory;
+import com.codecool.dungeoncrawl.logic.Item;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+import java.util.Map;
 
 public class StatusPane {
     public static final int RIGHT_PANEL_WIDTH = 200;
@@ -13,6 +18,8 @@ public class StatusPane {
     private Label healthValueLabel;
     private Label damageTextLabel;
     private Label damageValueLabel;
+    private VBox inventoryBox;
+
 
     public StatusPane() {
         ui = new GridPane();
@@ -20,6 +27,7 @@ public class StatusPane {
         healthValueLabel = new Label();
         damageTextLabel = new Label("Damage: ");
         damageValueLabel = new Label();
+        inventoryBox = new VBox();
     }
 
     public BorderPane build() {
@@ -33,6 +41,9 @@ public class StatusPane {
         ui.add(damageTextLabel,0,1);
         ui.add(damageValueLabel,1,1);
 
+        ui.add(new Label("Inventory"), 0 ,2);
+        ui.add(inventoryBox, 0, 3, 2, 1);
+
         BorderPane borderPane = new BorderPane();
         borderPane.setRight(ui);
         return borderPane;
@@ -43,5 +54,15 @@ public class StatusPane {
     }
     public void setDamageValue(String text) {
         damageValueLabel.setText(text);
+    }
+    public void updateInventory(Inventory inventory) {
+        inventoryBox.getChildren().clear();
+        Map<String, Integer> itemQuantities = inventory.getItemQuantities();
+        for (Map.Entry<String, Integer> entry : itemQuantities.entrySet()) {
+            String itemName = entry.getKey();
+            int quantity = entry.getValue();
+            Label itemLabel = new Label(itemName + ": " + quantity);
+            inventoryBox.getChildren().add(itemLabel);
+        }
     }
 }
